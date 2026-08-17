4025c6319c3ffc2aa5c8283df6bc5e6060542b84
kirillchertok <kirilka2005228@gmail.com>
2026-08-17 12:51:18 +0300

    feat: finish home page

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -6,6 +6,7 @@ const NotFound = lazy(() => import('@/pages/NotFound/NotFound'));
 export const ROUTES = [
     { path: '/', element: <Home />, displayName: 'Home' },
     { path: '/login', element: <Home />, displayName: 'Login' },
+    { path: '/locations', element: <Home />, displayName: 'Locations' },
     { path: '/shop', element: <Home />, displayName: 'Shop' },
     { path: '*', element: <NotFound /> },
 ];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
+import { HomeInfo } from '@/components/HomeInfo/HomeInfo';
+import { Button } from '@/components/ui/Button/Button';
 import { Layout } from '@/components/ui/Layout/Layout';
+
+import * as styles from './Home.module.css';
 
 const Home = () => {
     return (
         <Layout type='rice-flower'>
-            <div>asd</div>
+            <div className={styles.container}>
+                <h1 className={styles.header}>
+                    Rent your own field, invest in farming,
+                    <br />
+                    and grow your own vegetables
+                </h1>
+                <Button option='first'>Let&apos;s start</Button>
+                <HomeInfo />
+            </div>
         </Layout>
     );
 };
